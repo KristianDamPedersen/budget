0af3ef5e783e499298a6b3c1f9305e8b038fce43
JonathanDamDk <jonathan.dam.pedersen@gmail.com>
2025-12-14 17:52:31 +0100

    feat: Added I18n support

--- app/frontend/pages/inertia_example/index.tsx
+++ app/frontend/pages/inertia_example/index.tsx
@@ -1,16 +1,19 @@
 import { Head } from '@inertiajs/react'
-
+import { usePage } from '@inertiajs/react'
 import cs from './index.module.css'
 import { Button } from '@/components/ui/button'
-import { ModeToggle } from '@/components/ui/mode-toggle'
+import { i18n_t } from '@/lib/utils'
 
 export default function InertiaExample() {
+  const { i18n, green_text } = usePage().props
   return (
     <div className={cs.root}>
       <Head title="Ruby on Rails + Inertia + React" />
       <ModeToggle></ModeToggle>
       <div>
-        <Button> From ShadCn tailwind styled</Button>
+        <Button className="text-green-500">dynamic: {i18n_t(i18n, "pages.inertia_example.green_text")}</Button>
+        <Button className="text-green-500">static: {green_text}</Button>
+
       </div>
     </div>
   )
