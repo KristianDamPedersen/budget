09900ae7a6452e9f01aa433f1557daa7d3b64a1c
JonathanDamDk <jonathan.dam.pedersen@gmail.com>
2025-12-14 17:52:31 +0100

    feat: Added I18n support

--- app/controllers/inertia_controller.rb
+++ app/controllers/inertia_controller.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
 class InertiaController < ApplicationController
-  inertia_share flash: -> { flash.to_hash }
+  before_action :set_locale
+
+  inertia_share i18n: -> {
+    self.class.inertia_i18n_scopes.each_with_object({}) do |scope, acc|
+      section = I18n.t(scope, default: {})
+      next unless section.is_a?(Hash)
+
+      # Wrap the translated hash back under its scope path
+      wrapped = scope.to_s.split(".").reverse.reduce(section) { |h, key| { key => h } }
+      acc.deep_merge!(wrapped) # keep structure; later scopes override where they overlap
+    end
+  }
+
+
+  class_attribute :inertia_i18n_scopes, default: [ "common" ]
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
--- app/controllers/inertia_example_controller.rb
+++ app/controllers/inertia_example_controller.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class InertiaExampleController < InertiaController
+  self.inertia_i18n_scopes = [ "pages.inertia_example" ]
   def index
     render inertia: {
       rails_version: Rails.version,
       ruby_version: RUBY_DESCRIPTION,
       rack_version: Rack.release,
-      inertia_rails_version: InertiaRails::VERSION
+      inertia_rails_version: InertiaRails::VERSION,
+      green_text: I18n.t("pages.inertia_example.green_text")
     }
   end
 end
